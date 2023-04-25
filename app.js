de9f8a2bc86023018743cf74cebd57af8571ebb8
dhuha <junjer32@gmail.com>
2023-04-25 04:05:16 +0300

    Initialize junjer-patch-1

---
+++ app.js
@@ -0,0 +1,26 @@
+// Modules
+var express = require('express');
+var app = express();
+
+// Define port
+var port = 3000;
+
+// View engine
+app.set('view engine', 'jade');
+
+// Set public folder
+app.use(express.static(__dirname + '/public'));
+
+// Rest
+var rest = require("arest")(app);
+rest.addDevice('xbee','/dev/tty.usbserial-A702LF8B');
+
+// Serve interface
+app.get('/', function(req, res){
+  var devices = rest.getDevices();
+  res.render('interface', {devices: devices});
+});
+
+// Start server
+app.listen(port);
+console.log("Listening on port " + port);
